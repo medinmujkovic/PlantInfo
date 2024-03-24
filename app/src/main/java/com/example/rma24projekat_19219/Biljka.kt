2b218a248bd3730a6255f3d3512ad0f5dbcf3ed8
medinmujkovic <mmujkovic1@etf.unsa.ba>
2024-03-24 23:32:21 +0100

    added classes

---
+++ app/src/main/java/com/example/rma24projekat_19219/Biljka.kt
@@ -0,0 +1,12 @@
+package com.example.rma24projekat_19219
+
+data class Biljka(
+    val naziv: String,
+    val porodica: String,
+    val medicinskoUpozorenje: String,
+    val medicinskeKoristi: List<MedicinskaKorist>,
+    val profilOkusa: ProfilOkusaBiljke,
+    val jela: List<String>,
+    val klimatskiTipovi: List<KlimatskiTip>,
+    val zemljisniTipovi: List<Zemljiste>
+)
---
+++ app/src/main/java/com/example/rma24projekat_19219/Zemljiste.kt
@@ -0,0 +1,10 @@
+package com.example.rma24projekat_19219
+
+enum class Zemljiste(val naziv: String) {
+    PJESKOVITO("Pjeskovito zemljište"),
+    GLINENO("Glinеno zemljište"),
+    ILOVACA("Ilovača"),
+    CRNICA("Crnica"),
+    SLJUNOVITO("Šljunovito zemljište"),
+    KRECNJACKO("Krečnjačko zemljište");
+}
